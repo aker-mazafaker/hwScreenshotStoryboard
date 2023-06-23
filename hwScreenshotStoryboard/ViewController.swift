81eb4212eb1f4a9402d874d8859884a3786a1e72
Aker <@akerke.n@inbox.ru>
2023-06-23 13:52:46 +0600

    Initial Commit

---
+++ hwScreenshotStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hwScreenshotStoryboard
+//
+//  Created by Akerke on 23.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
